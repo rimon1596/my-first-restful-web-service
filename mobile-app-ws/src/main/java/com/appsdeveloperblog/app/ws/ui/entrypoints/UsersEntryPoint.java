bcb9e23a95d588047e92e79c1f5c0c5166573039
simonoreilly <simonoreilly@simons-mbp>
2020-05-05 22:35:59 +0100

    set up the service layer and new skeleton methods for creating user

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/entrypoints/UsersEntryPoint.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/entrypoints/UsersEntryPoint.java
@@ -6,6 +6,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.BeanUtils;
+
+import com.appsdeveloperblog.app.ws.service.UsersService;
+import com.appsdeveloperblog.app.ws.service.impl.UsersServiceImpl;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDTO;
 import com.appsdeveloperblog.app.ws.ui.model.request.CreateUserRequestModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserProfileRest;
 
@@ -16,10 +21,20 @@ public class UsersEntryPoint {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	
 	public UserProfileRest createUser(CreateUserRequestModel requestObject) {
 		
 		UserProfileRest returnValue = new UserProfileRest();
+		
+		// Prepare userDTO
+		UserDTO userDto = new UserDTO();
+		BeanUtils.copyProperties(requestObject, userDto);
+		
+		//create new user
+		UsersService userService = new UsersServiceImpl();
+		UserDTO createdUserProfile = userService.createUser(userDto);
+		
+		//prepare response
+		BeanUtils.copyProperties(createdUserProfile, returnValue);
 		
 	    return returnValue;	
 	}
